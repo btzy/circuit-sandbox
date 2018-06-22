f52a8be6f3583eaa30b6aa5ad7775063172903e5
Kuan Wei Heng <kuanwh1996@gmail.com>
2018-06-22 12:31:54 +0800

    Make HistoryAction a subclass of EditAction

--- CircuitPlayground/historyaction.hpp
+++ CircuitPlayground/historyaction.hpp
@@ -3,33 +3,15 @@
 #include "baseaction.hpp"
 #include "playarea.hpp"
 
-class HistoryAction final : public BaseAction {
-
-protected:
-    extensions::point deltaTrans;
-    PlayArea& playArea;
-    bool const simulatorRunning;
+class HistoryAction final : public EditAction {
 
 public:
 
-    HistoryAction(PlayArea& playArea) :deltaTrans({ 0, 0 }), playArea(playArea), simulatorRunning(playArea.stateManager.simulator.running()) {
-        // stop the simulator if running
-        if (simulatorRunning) playArea.stateManager.simulator.stop();
-        playArea.stateManager.updateDefaultState();
-    };
-
-    CanvasState& canvas() const {
-        return playArea.stateManager.defaultState;
-    }
+    HistoryAction(PlayArea& playArea) : EditAction(playArea) {};
 
     ~HistoryAction() override {
-        // amend the translation for playArea and stateManager
+        // reset the translation for playArea and stateManager
         playArea.stateManager.deltaTrans = { 0, 0 };
-        playArea.translation -= deltaTrans * playArea.scale;
-        // recompile the simulator
-        playArea.stateManager.simulator.compile(canvas(), false);
-        // start the simulator if its supposed to be running
-        if (simulatorRunning) playArea.stateManager.simulator.start();
     }
 
     // check if we need to start selection action from Ctrl-Z or Ctrl-Y
