6c5f04b1e8309a945078e23f0b42dcd952b46fdb
Bernard Teo <bernard.14916@gmail.com>
2018-08-04 20:56:11 +0800

    Implement reference counting for clipboard shared memory on POSIX
    - This is a not-very-ideal solution for process-persistant shared memory on linux (#43).
    - It is implemented by incrementing a counter stored on the shared memory when the shared memory is created or opened, and decrementing the counter when the shared memory is closed (the process that decremented the counter to zero is responsible for removing the shared memory).
    - If Circuit Sandbox crashes or is otherwise killed, the shared memory won't be removed until a system reboot.

--- CircuitSandbox/shared_memory.hpp
+++ CircuitSandbox/shared_memory.hpp
@@ -11,39 +11,78 @@
 namespace ext {
     /**
      * Represents a shared memory buffer that will be deleted when no longer referenced by any process.
-     * New memory is zeroed by POSIX ftruncate().
+     * New memory is zeroed by POSIX API ftruncate() - called by boost::interprocess::shared_memory_object::truncate.
      */
     class autoremove_shared_memory {
     private:
         boost::interprocess::shared_memory_object shm;
         boost::interprocess::mapped_region region;
+        using counter_t = std::atomic<std::size_t>;
+        static_assert(counter_t::is_always_lock_free, "Need always lock-free atomics!");
+        std::size_t counter_offset;
         template <typename OpenMode>
         autoremove_shared_memory(const char* name, OpenMode open_mode, boost::interprocess::mode_t mode) :
             shm(open_mode, name, mode) {
+        }
+        std::size_t ref_increment() noexcept {
+            counter_t* counter = reinterpret_cast<counter_t*>(static_cast<char*>(region.get_address()) + counter_offset);
+            return counter->fetch_add(1, std::memory_order_acq_rel);
+        }
+        std::size_t ref_decrement() noexcept {
+            counter_t* counter = reinterpret_cast<counter_t*>(static_cast<char*>(region.get_address()) + counter_offset);
+            return counter->fetch_sub(1, std::memory_order_acq_rel);
         }
     public:
         using size_type = boost::interprocess::offset_t;
         template <typename OpenMode = boost::interprocess::open_or_create_t>
         autoremove_shared_memory(const char* name, size_type size, OpenMode open_mode = boost::interprocess::open_or_create, boost::interprocess::mode_t mode = boost::interprocess::read_write) :
-            autoremove_shared_memory(name, open_mode, mode) {
+            autoremove_shared_memory(name, open_mode, boost::interprocess::read_write) {
             // as this is a delegating constructor, the destructor will be called if this constructor throws.
-            size_type existing_size = 0;
-            shm.get_size(existing_size);
-            if (existing_size < size) {
-                shm.truncate(size);
+            
+            assert(mode == boost::interprocess::read_write || mode == boost::interprocess::read_only);
+            // note: on posix, the mode is ignored because we need write access to do reference counting.
+
+            while (true) {
+                // add space for the reference counter after the data
+                size = ((size - 1) / alignof(counter_t) + 1) * alignof(counter_t);
+                counter_offset = static_cast<std::size_t>(size);
+                size += sizeof(counter_t);
+
+                size_type existing_size = 0;
+                shm.get_size(existing_size);
+                if (existing_size < size) {
+                    shm.truncate(size);
+                }
+                region = boost::interprocess::mapped_region(shm, boost::interprocess::read_write);
+
+                // increment the reference counter
+                if (ref_increment() == 0 && existing_size > 0) {
+                    // if we got here, it means that we are holding some old memory that's just about to be deleted
+                    // so we remove the old memory and retry.
+                    region = boost::interprocess::mapped_region();
+                    shm = boost::interprocess::shared_memory_object();
+                    boost::interprocess::shared_memory_object::remove(name);
+                    // create a new shared memory object, now we should have fresh new memory.
+                    shm = boost::interprocess::shared_memory_object(open_mode, name, boost::interprocess::read_write);
+                    continue;
+                }
+
+                break;
             }
-            region = boost::interprocess::mapped_region(shm, mode);
         }
-        autoremove_shared_memory() {}
-        ~autoremove_shared_memory() {
-            // unbind the region
-            region = boost::interprocess::mapped_region();
-            // get the name
-            std::string name(shm.get_name());
-            // destroy the shared memory
-            shm = boost::interprocess::shared_memory_object();
-            // remove the memory (if possible)
-            boost::interprocess::shared_memory_object::remove(name.c_str());
+        autoremove_shared_memory() noexcept {}
+        ~autoremove_shared_memory() noexcept {
+            // check if we are the last user of this shared memory, and if so we remove the memory.
+            if (ref_decrement() == 1) {
+                // unbind the region
+                region = boost::interprocess::mapped_region();
+                // get the name
+                std::string name(shm.get_name());
+                // destroy the shared memory
+                shm = boost::interprocess::shared_memory_object();
+                // remove the memory (if possible)
+                boost::interprocess::shared_memory_object::remove(name.c_str());
+            }
         }
         void* address() const noexcept {
             return region.get_address();
@@ -62,7 +101,7 @@ namespace ext {
 namespace ext {
     /**
      * Represents a shared memory buffer that will be deleted when no longer referenced by any process.
-     * New memory is zeroed by Windows API CreateFileMappingA().
+     * New memory is zeroed by Windows API CreateFileMappingA() - called by boost::interprocess::windows_shared_memory::windows_shared_memory().
      */
     class autoremove_shared_memory {
     private:
